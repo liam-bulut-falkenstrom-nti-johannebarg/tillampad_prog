18c25211ee24bfad79e6e414d7e90ad19446cb62
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2021-11-12 09:01:46 +0100

    la till lite saker

--- game.rb
+++ game.rb
@@ -1,1 +1,54 @@
 # Hej klassen! Välkommen till spelet!
+require 'ruby2d'
+require_relative "setup_var.rb"
+require_relative "help_functions.rb"
+
+set width: 400
+set height: 240
+set resizable: true
+
+on :key_held do |event|
+    case event.key
+        when 'd'
+            @x_dir = 1
+            @right = 1
+        when 'a'
+            @x_dir = -1
+            @left = 1
+        when 'w'
+            @y_dir = -1
+            @up = 1
+        when 's'
+            @y_dir = 1
+            @down = 1
+    end     
+end
+
+on :key_up do |event|
+    case event.key
+        when 'd'
+            @right = 0
+        when 'a'
+            @left = 0
+        when 'w'
+            @up = 0
+        when 's'
+            @down = 0
+    end     
+end
+
+update do
+    if (@right - @left).abs == (@down - @up).abs
+        @x_dir = (@right - @left) * 0.70 # typ roten ur 2 dividerat på 2
+        @y_dir = (@down - @up) * 0.70 
+    else
+        @x_dir = (@right - @left) 
+        @y_dir = (@down - @up)
+    end 
+    @player.x += @x_dir * @walk_speed
+    @player.y += @y_dir * @walk_speed
+
+
+end
+
+show
---
+++ setup_var.rb
@@ -0,0 +1,19 @@
+#Sprites
+@player = Sprite.new('sprites\player_sprite.png',
+    clip_width: 16,
+    width: 16,
+    height: 16,
+    x: 100,
+    y: 100,
+    loop: true,
+    time: 100,
+)
+
+@left = 0
+@right = 0
+@x_dir = 0
+@flipped = false
+@up = 0
+@down = 0
+@y_dir = 0
+@walk_speed = 1.5
