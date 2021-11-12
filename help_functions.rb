b2c488c9e498200da6e3fa32642c13df8441bbf4
liam-bulut-falkenstrom-nti-johannebarg <liam.bulutfalkenstrom@elev.ga.ntig.se>
2021-11-12 13:55:00 +0100

    unit vector

--- help_functions.rb
+++ help_functions.rb
@@ -1,12 +1,14 @@
 def mouse_angle(mouse_x, mouse_y, player_center_array)
     dist_x = mouse_x - player_center_array[0]
     dist_y = mouse_y - player_center_array[1]
+    vector = Math.sqrt(dist_x**2 + dist_y**2)
+    angle = Math.atan(dist_y/dist_x) * (180/(Math::PI))
+    unit_vector = [dist_x/vector, dist_y/vector]
     if dist_x >= 0
-        angle = Math.atan(dist_y/dist_x) * (180/(Math::PI))
-        return [angle, dist_x, dist_y] 
+        return [angle, unit_vector[0], unit_vector[1]] 
     else
         angle = -90 - (90 - Math.atan(dist_y/dist_x) * (180/(Math::PI))) 
-        return [angle, dist_x, dist_y]
+        return [angle, unit_vector[0], unit_vector[1]]
     end 
 end
 
